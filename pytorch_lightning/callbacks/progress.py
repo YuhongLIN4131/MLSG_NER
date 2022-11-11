40bdf706390af40beba574c2595060d704a3ef0b
YuhongLIN4131 <1450422779@qq.com>
2022-11-11 11:37:24 +0800

    第5个epoch开始训练

--- pytorch_lightning/callbacks/progress.py
+++ pytorch_lightning/callbacks/progress.py
@@ -114,7 +114,7 @@ class ProgressBarBase(Callback):
         total_val_batches = 0
         if not self.trainer.disable_validation:
             is_val_epoch = (self.trainer.current_epoch) % self.trainer.check_val_every_n_epoch == 0
-            total_val_batches = sum(self.trainer.num_val_batches) if (is_val_epoch and self.trainer.current_epoch>3) else 0
+            total_val_batches = sum(self.trainer.num_val_batches) if (is_val_epoch and self.trainer.current_epoch>5) else 0
         return total_val_batches
 
     @property
